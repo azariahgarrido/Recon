db41a5cd5f4901e004d8eeca9236317a84458035
azariahgarrido <azariahdanielgarrido@gmail.com>
2022-10-04 18:13:34 -0700

    Saved-Books not rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,71 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import SearchBooks from './pages/SearchBooks';
+// import SavedBooks from './pages/SavedBooks';
+// import Navbar from './components/Navbar';
+
+// function App() {
+//   return (
+//     <Router>
+//       <>
+//         <Navbar />
+//         <Routes>
+//           <Route 
+//             path='/' 
+//             element={<SearchBooks />} 
+//           />
+//           <Route 
+//             path='/saved' 
+//             element={<SavedBooks />} 
+//           />
+//           <Route 
+//             path='*'
+//             element={<h1 className='display-2'>Wrong page!</h1>}
+//           />
+//         </Routes>
+//       </>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={<SearchBooks />} />
+            <Route exact path='/saved' element={<SavedBooks />} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
